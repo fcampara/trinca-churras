0df3aaa39eb41cc96c7b7bbd56254b2e3b592381
Felipe Campara <fcamparasilva@gmail.com>
2023-11-16 20:44:01 -0300

    feat(input): create new component input

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,15 @@
+import { FC, useId } from "react";
+import styles from '@/components/Input/input.module.scss'
+import { InputProps } from "@/components/Input/input.types";
+
+const Input: FC<InputProps> = (props) => {
+  const { label, ...restProps } = props;
+
+  return (
+      <label className={styles["tr-input__label"]} >
+        {label}
+        <input {...restProps} className={styles["tr-input"]} />
+      </label>
+  );
+};
+export default Input;
